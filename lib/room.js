lpinca/binb

commit 30cec4c177df6ab54b044ec01fbd7828fef9ed9d

use a PRNG based on `crypto.randomBytes` to play tracks randomly

lib/room.js
@@ -8,10 +8,13 @@ var amatch = require('./match')
   , config = require('../config')
   , fifolength = config.songsinarun * config.gameswithnorepeats
   , io
-  , isUsername = require('./utils').isUsername
+  , randInt = require('./prng').randInt
   , sockets
   , songsdb = clients.songs
-  , usersdb = clients.users;
+  , usersdb = clients.users
+  , utils = require('./utils')
+  , isUsername = utils.isUsername
+  , trackscount = utils.trackscount;
 
 /**
  * Expose the constructor.
@@ -42,7 +45,6 @@ function Room(roomname) {
     , status
     , title // Title in lowercase
     , trackName
-    , trackscount = 0
     , trackViewUrl
     , totusers = 0
     , usersData = Object.create(null);
@@ -135,7 +137,7 @@ function Room(roomname) {
     totusers++;
     // Broadcast new user event
     io.sockets.emit('updateoverview', roomname, totusers);
-    socket.emit('ready', usersData, trackscount, loggedin);
+    socket.emit('ready', usersData, trackscount[roomname], loggedin);
     socket.broadcast.to(roomname).emit('newuser', socket.nickname, usersData);
   };
 
@@ -331,7 +333,8 @@ function Room(roomname) {
 
   // Extract a random track from the database and send the load event
   var sendLoadTrack = function() {
-    songsdb.srandmember(roomname, function(err, res) {
+    var index = randInt(trackscount[roomname]);
+    songsdb.zrange(roomname, index, index, function(err, res) {
       // Check if extracted track is in the list of already played tracks
       if (~playedtracks.indexOf(res)) {
         return sendLoadTrack();
@@ -451,10 +454,10 @@ function Room(roomname) {
 
   // Start the room
   this.start = function() {
-    songsdb.scard(roomname, function(err, res) {
-      trackscount = res;
+    songsdb.zcard(roomname, function(err, card) {
+      trackscount[roomname] = card;
+      sendLoadTrack();
     });
-    sendLoadTrack();
   };
 
   this.unignore = function(who, executor) {

routes/site.js
@@ -5,8 +5,11 @@
 var async = require('async')
   , Captcha = require('../lib/captcha')
   , db = require('../lib/redis-clients').songs
-  , randomSlogan = require('../lib/utils').randomSlogan
-  , rooms = require('../config').rooms;
+  , randInt = require('../lib/prng').randInt
+  , rooms = require('../config').rooms
+  , utils = require('../lib/utils')
+  , randomSlogan = utils.randomSlogan
+  , trackscount = utils.trackscount;
 
 /**
  * Generate a task.
@@ -14,7 +17,8 @@ var async = require('async')
 
 var task = function(genre) {
   return function(callback) {
-    db.srandmember(genre, function(err, res) {
+    var index = randInt(trackscount[genre]);
+    db.zrange(genre, index, index, function(err, res) {
       db.hget('song:'+res, 'artworkUrl100', callback);
     });
   };

lib/prng.js
@@ -1,0 +1,30 @@
+/**
+ * Module dependencies.
+ */
+
+var crypto = require('crypto')
+  , rrange = 4294967296;
+
+/**
+ * Return an integer, pseudo-random number in the range [0, 2^32).
+ */
+
+var nextInt = function() {
+  return crypto.randomBytes(4).readUInt32BE(0);
+};
+
+/**
+ * Return a floating-point, pseudo-random number in the range [0, 1).
+ */
+
+var rand = function() {
+  return nextInt() / rrange;
+};
+
+/**
+ * Return an integer, pseudo-random number in the range [0, max).
+ */
+
+exports.randInt = function(max) {
+  return Math.floor(rand() * max);
+};

lib/utils.js
@@ -40,6 +40,12 @@ exports.britishFormat = function(date) {
 };
 
 /**
+ * Helper object for faster and easier access to rooms' cardinalities (number of tracks).
+ */
+
+exports.trackscount = {};
+
+/**
  * Check whether a given string is a valid email address.
  */
 

util/load_sample_tracks.js
@@ -12,6 +12,7 @@ var artistIds = require('./artist-ids')
   , rc = require('redis').createClient()
   , rockIds = artistIds.rock
   , rooms = require('../config').rooms
+  , score
   , skip = 0 // Skip counter
   , songId = 0;
 
@@ -29,6 +30,7 @@ var options = {
 
 var updateRooms = function(artistId) {
   rooms = ['mixed'];
+  score = 0;
   if (artistId === popIds[0]) {
     rooms.push('hits', 'pop');
     // Set the skip counter (there is no need to update the rooms for the next pop artists)
@@ -64,9 +66,11 @@ parser.on('data', function(track) {
   );
 
   rooms.forEach(function(room) {
-    rc.sadd(room, songId);
+    var _score = (room === 'mixed') ? songId : score;
+    rc.zadd(room, _score, songId);
   });
 
+  score++;
   songId++;
 });
 

commit 7b6daa025e29d3d1e6356b7c3b9ae777e7f273a3

fixed a bug introduced in commit 30cec4c

lib/room.js
@@ -335,13 +335,14 @@ function Room(roomname) {
   var sendLoadTrack = function() {
     var index = randInt(trackscount[roomname]);
     songsdb.zrange(roomname, index, index, function(err, res) {
+      var id = res[0];
       // Check if extracted track is in the list of already played tracks
-      if (~playedtracks.indexOf(res)) {
+      if (~playedtracks.indexOf(id)) {
         return sendLoadTrack();
       }
-      playedtracks.push(res);
+      playedtracks.push(id);
       var args = [
-        'song:'+res
+        'song:'+id
         , 'artistName'
         , 'trackName'
         , 'previewUrl'

routes/site.js
@@ -19,7 +19,7 @@ var subTask = function(genre) {
   return function(callback) {
     var index = randInt(trackscount[genre]);
     db.zrange(genre, index, index, function(err, res) {
-      db.hget('song:'+res, 'artworkUrl100', callback);
+      db.hget('song:'+res[0], 'artworkUrl100', callback);
     });
   };
 };